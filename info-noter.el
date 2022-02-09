7c32b91d80ea36ce29cc7c34094d14d4aed1f429
Emin Umut Gerçek <umutgercek1999@gmail.com>
2022-02-09 18:04:20 +0300

    Refactor code

--- info-noter.el
+++ info-noter.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: info-mode org-mode workflow
 ;; Homepage: https://github.com/eugercek/info-note
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "27.2"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,34 +19,78 @@
 ;;
 ;;; Code:
 
-(defun info-heading->org-heading (arg)
-  "Send current heading to an org-mode buffer.
-Buffer is chosen from the `info-noter-org-file`.
-To change already setted `info-noter-org-file` call function with some
-argument like \\[universal-argument].
-To set the variable just call the function."
+;;; Variables
+
+(defvar info-noter/notes-dir
+  "Base path for info-noter notes.")
+
+(defvar info-noter/current-note-file
+  "Currently note file.")
+
+
+;;; Interactive functions
+
+(defun info-noter/setup (arg)
+  "Start info-noter session.
+Choose a file no take notes on.
+Give ARG for choose from the directory `info-noter/notes-dir'
+If: Only one org buffer is visible choose that.
+Else: Choose selectively."
+
   (interactive "P")
-  (let ((text (thing-at-point 'line t))
-        (buf (get-info-noter-org-file-name arg)))
-    (with-current-buffer buf
+  (info-noter--set-info-file arg))
+
+(defun info-noter/copy-heading ()
+  "Send current heading to `info-noter/note-file' file.
+To change already setted `info-noter-org-file' call function with some
+ARG like \\[universal-argument]."
+  (interactive)
+  (let ((text (thing-at-point 'line t)))
+    (with-current-buffer info-noter/current-note-file
       (goto-char (point-max))
       (insert text)
       (forward-line -1)
       (org-ctrl-c-star))))
 
-(defun get-info-noter-org-file-name (arg)
-  "Gets `info-noter-org-file`.
-If it's not sett yet user prompted to choose.
-If it's already selected it'll used."
-  (defun ask ()
-    (setq info-noter-org-file
-          (file-name-nondirectory
-           (read-file-name
-            "Select note file:"))))
+(defun info-noter/copy-text ()
+  "TODO.
+If ARG choose again."
+  (interactive)
+  (let ((text (thing-at-point 'sentence t)))
+    (with-current-buffer info-noter/current-note-file
+      (goto-char (point-max))
+      (insert text)
+      (newline))))
 
-  (cond ((not  (eq arg nil)) (ask))
-        ((boundp 'info-noter-org-file) info-noter-org-file)
-        (t (ask))))
+
+;;; Helper Functions
+
+(defun info-noter--set-info-file (arg)
+  "Set info file."
+  (setq info-noter/current-note-file
+        (if arg
+            (expand-file-name
+             (read-file-name
+              "Choose note file:"
+              info-noter/notes-dir))
+          (let ((buffer-list (info-noter--get-visible-note-buffers "org")))
+            (cond ((eq (length buffer-list) 1)
+                   (car buffer-list))
+                  ((eq (length buffer-list) 1)
+                   (completing-read "Choose note file:"
+                                    buffer-list))))))
+  (message "Selected %s" info-noter/current-note-file))
+
+(defun info-noter--get-visible-note-buffers (extension)
+  "Get visible buffers that has EXTENSION as extension."
+  (seq-filter (lambda (buffer)
+                (and
+                 (not (eq (current-buffer)
+                          buffer))
+                 (string= (file-name-extension
+                           (buffer-name buffer))
+                          extension)))
+              (map 'list #'window-buffer (window-list))))
 
 (provide 'info-noter)
 ;;; info-note.el ends here
